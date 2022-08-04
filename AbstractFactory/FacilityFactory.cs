c4d484c265db69bfaf5c79a9a37bd210f10d4064
Mingxuan <meticals@icloud.com>
2022-08-04 11:37:39 +0800

    no message

---
+++ AbstractFactory/FacilityFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace Creational_AbstractFactory
+{
+    // 道路
+    public abstract class Road
+    {
+        public abstract void Log();
+    }
+
+    // 房屋
+    public abstract class Building
+    {
+        public abstract void Log();
+    }
+
+    // 地道
+    public abstract class Tunnel
+    {
+        public abstract void Log();
+    }
+
+    // 丛林
+    public abstract class Jungle
+    {
+        public abstract void Log();
+    }
+
+    public abstract class FacilityFactory
+    {
+        public abstract Road CreateRoad();
+        public abstract Building CreateBuilding();
+        public abstract Tunnel CreateTunnel();
+        public abstract Jungle CreateJungle();
+    }
+
+}
